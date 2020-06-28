512d913db5fa0bb2da5811677048df0965d5e887
satoshi.sobue <2025opsmg2o@gmail.com>
2020-06-29 01:53:19 +0900

    105, 106th contest

---
+++ A/106.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector> 
+#include <math.h> 
+using namespace std;
+
+int main(void) {
+    int A,B; cin >> A >> B;
+    cout << (A * B) - A - B + 1 << endl;
+}
---
+++ A/107.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector> 
+#include <math.h> 
+using namespace std;
+
+int main(void) {
+    int N, i; cin >> N >> i;
+    cout << N - i + 1 << endl;
+}
