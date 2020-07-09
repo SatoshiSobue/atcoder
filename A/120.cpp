7b43383b6dd7701c0fb700bc88fa2ec75e99e842
satoshi.sobue <2025opsmg2o@gmail.com>
2020-07-09 17:46:58 +0900

    120th contest

---
+++ A/120.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector> 
+#include <math.h> 
+using namespace std;
+
+int main(void) {
+    int a,b,c; cin >> a >> b >> c;
+    cout << min(b/a, c) << endl;
+}
